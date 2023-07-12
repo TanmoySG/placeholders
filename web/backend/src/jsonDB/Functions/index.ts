dfb183e77367976f8021a5170d657b08f6ea429c
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2023-07-12 21:26:00 +0530

    chore(user-register): Error handling on addNamespaceTableToDB
    Fixed #10

--- web/backend/src/jsonDB/Functions/index.ts
+++ web/backend/src/jsonDB/Functions/index.ts
@@ -7,17 +7,21 @@ const USER_TABLE = "ph-systems-users";
 const NAMESPACE_TABLE = "ph-namespaces";
 
 export const addNamespaceTableToDB = async (username: string): Promise<void> => {
-  const jsonData = await fs.readFile(JSON_DB_LOCATION, "utf-8");
-  const parsedData = JSON.parse(jsonData);
-  parsedData[`${NAMESPACE_TABLE}_${username}-default-namespace`] = [];
-  console.log(parsedData);
-  const updatedJsonData = JSON.stringify(parsedData, null, 2);
-  await fs.writeFile(JSON_DB_LOCATION, updatedJsonData);
+  try {
+    const jsonData = await fs.readFile(JSON_DB_LOCATION, "utf-8");
+    const parsedData = JSON.parse(jsonData);
+    parsedData[`${NAMESPACE_TABLE}_${username}-default-namespace`] = [];
+    console.log(parsedData);
+    const updatedJsonData = JSON.stringify(parsedData, null, 2);
+    await fs.writeFile(JSON_DB_LOCATION, updatedJsonData);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const addUser = async (user: any): Promise<any> => {
-  const addedUser = await axios.post(`http://localhost:5001/${USER_TABLE}`, { ...user });
-  return addedUser.data;
+  const { data } = await axios.post(`${JSON_DB_BASE_URL}/${USER_TABLE}`, { ...user });
+  return data;
 };
 
 export const getAllUsersWithUsername = async (username: string): Promise<any> => {
